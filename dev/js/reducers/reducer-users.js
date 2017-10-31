01e033afc2ec5496deec4ff7e2d5026d087fc2ce
Gh05d <pascal.clanget@googlemail.com>
2017-10-31 11:58:44 +0100

    As you wished

---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,28 @@
+export default function() {
+  return [
+    {
+      id: 1,
+      first: "Bucky",
+      last: "Roberts",
+      age: 71,
+      description: "Bucky is my teacher",
+      thumbnail: "http://i.imgur.com/7yUvePI.jpg"
+    },
+    {
+      id: 2,
+      first: "Pascal",
+      last: "Cousland",
+      age: 23,
+      description: "He is the awesome pupil",
+      thumbnail: "http://i.imgur.com/52xRlm8.png"
+    },
+    {
+      id: 3,
+      first: "Daisy",
+      last: "Roberts",
+      age: 71,
+      description: "Buckies wife",
+      thumbnail: "http://i.imgur.com/4EMtxHB.png"
+    },
+  ]
+}
